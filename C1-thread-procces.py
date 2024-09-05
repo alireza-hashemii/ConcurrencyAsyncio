03e5e4d5bb641d50d631b5b6795084d55fa3b900
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-09-05 09:26:35 +0330

    concepts and introduction: 1-parallelism - thread - procces - ...

---
+++ C1-thread-procces.py
@@ -0,0 +1,23 @@
+# import threading
+# import os
+
+# print(f'Python process running with process id: {os.getpid()}')
+
+# total_threads = threading.active_count()
+# thread_name = threading.current_thread().name
+# print(f'Python is currently running {total_threads} thread(s)')
+# print(f'The current thread is {thread_name}')
+
+
+
+import threading
+def hello_from_thread():
+    print(f'Hello from thread {threading.current_thread()}!')
+
+hello_thread = threading.Thread(target=hello_from_thread)
+hello_thread.start()
+total_threads = threading.active_count()
+thread_name = threading.current_thread().name
+print(f'Python is currently running {total_threads} thread(s)')
+print(f'The current thread is {thread_name}')
+hello_thread.join()
